f0d0a53125fcc1f489603966119e242a7ed16beb
Hikaru Suido <hikaru.suido@gmail.com>
2018-06-09 14:11:12 +0900

    Update

---
+++ vec.h
@@ -0,0 +1,59 @@
+#ifndef VEC_H
+#define VEC_H
+
+#include <cmath>
+#include <iostream>
+
+struct Vec {
+	float x, y, z;
+
+	Vec() {
+		x=y=z=0;
+	};
+
+	Vec(float x_, float y_, float z_) {
+		x=x_; y=y_; z=z_;
+	};
+
+	Vec operator+(const Vec &b) const {
+		return Vec(x+(b.x), y+(b.y), z+(b.z));
+	};
+
+	Vec operator-(const Vec &b) const {
+		return Vec(x-(b.x), y-(b.y), z-(b.z));
+	};
+
+	Vec operator*(float b) const {
+		return Vec(x*b, y*b, x*b);
+	};
+
+    Vec operator/(float b) const {
+        return Vec(x/b, y/b, z/b);
+    };
+
+	Vec mult(const Vec &b) const {
+		return Vec(x*(b.x), y*(b.y), z*(b.z));
+	};
+
+	float abs() const {
+		return sqrt(x*x+y*y+z*z);
+	};
+
+	Vec norm() {
+		return *this = *this * (1/sqrt(x*x+y*y+z*z));
+	};
+
+	float dot(const Vec &b) const {
+		return x*(b.x)+y*(b.y)+z*(b.z);
+	};
+
+	Vec cross(Vec &b) {
+		return Vec(y*(b.z)-z*(b.y), z*(b.x)-x*(b.z), x*(b.y)-y*(b.x));
+	};
+    
+    void print() const {
+        std::cout << "(" << x << ", " << y << ", " << z << ")\n";
+    };
+};
+
+#endif
