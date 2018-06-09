00f6875191c60adc972dec84609edb2a05d016fe
Hikaru Suido <hikaru.suido@gmail.com>
2018-06-09 15:30:14 +0900

    Update

---
+++ camera.h
@@ -0,0 +1,25 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "vec.h"
+#include "ray.h"
+
+struct Camera {
+    Vec camPos;
+    Vec camDir;
+    Vec camHor;
+    Vec camVer;
+
+    Camera(const Vec camPos_, const Vec camDir_) : camPos(camPos_), camDir(camDir_) {
+        camHor = camDir.cross(Vec(0, 1, 0)).norm() * -1;
+        camVer = camDir.cross(camHor).norm();
+    };
+
+    Ray getRay(float u, float v) const {
+        Vec pinhole = camPos + camDir;
+        Vec sensor = camPos + camHor * u + camVer * v;
+        return Ray(sensor, (pinhole - sensor).norm());
+    };
+};
+
+#endif
