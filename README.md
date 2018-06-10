f145489d13ffbf3219af27cdb292d528556a541c
Hikaru Suido <hikaru.suido@gmail.com>
2018-06-11 06:16:27 +0900

    Update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # renderer
+- ray tracing
--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 int main() {
     Image img(512, 512);
     Camera cam(Vec(0., 0., 0.), Vec(0., 0., 1.));
-    Sphere sph(1.0, Vec(0., 0., 5.));
+    Sphere sph(1.0, Vec(0., 0., 3.));
 
     for(int y=1; y<=img.height; y++) {
         for(int x=1; x<=img.width; x++) {
