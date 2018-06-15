1c233265d3ff3d66558f3e977d7b023f596fda91
Hikaru Suido <hikaru.suido@gmail.com>
2018-06-15 11:20:49 +0900

    Update

--- hit.h
+++ hit.h
@@ -1,14 +1,15 @@
 #ifndef HIT_H
 #define HIT_H
+
 #include "vec.h"
 
 struct Hit {
-        float t; //衝突点までの距離
-        Vec hitPos; //衝突位置
-        Vec hitNorm; //衝突位置の法線
+        float hitDis;
+        Vec hitPos;
+        Vec hitNorm;
 
         Hit() {};
-        Hit(double t, const Vec &hitPos, const Vec &hitNorm) : t(t), hitPos(hitPos), hitNorm(hitNorm) {};
+        Hit(float hitDis_, const Vec &hitPos_, const Vec &hitNorm_) : hitDis(hitDis_), hitPos(hitPos_), hitNorm(hitNorm_) {};
 };
 
 #endif
---
+++ test.cpp
@@ -0,0 +1,6 @@
+#include <omp.h>
+
+int main() {
+	return 0;
+}
+
