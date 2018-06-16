708cd8fc965df8333c55cca3d335764ba5bd6623
Hikaru Suido <hikaru.suido@gmail.com>
2018-06-16 14:57:01 +0900

    Update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <memory>
 #include <iostream>
+#include <random>
 #include "vec.h"
 #include "ray.h"
 #include "sphere.h"
@@ -8,6 +9,14 @@
 #include "camera.h"
 #include "hit.h"
 #include "accel.h"
+
+std::random_device dev_rnd;
+std::mt19937 mt(dev_rnd());
+std::uniform_real_distribution<> dist(0, 1);
+
+float rnd() {
+    return dist(mt);
+}
 
 Vec reflect(const Vec& d, const Vec& n) {
     return d - n * 2 * d.dot(n);
@@ -20,38 +29,53 @@ int main() {
     Sphere sph(3.0, Vec(0., 0., 3.));
     Accel accel;
     accel.add(std::make_shared<Sphere>(Sphere(1., Vec(0, 0, 0))));
-    accel.add(std::make_shared<Sphere>(Sphere(1000., Vec(0, -1001, 0))));
+    accel.add(std::make_shared<Sphere>(Sphere(10000., Vec(0, -10001, 0))));
 
     Vec lightDir = Vec(-1, 0.5, -1).norm();
 
-    for(int y=1; y<=img.height; y++) {
-        for(int x=1; x<=img.width; x++) {
-            float u = (2.0*x - img.width)/img.width;
-            float v = (2.0*y - img.height)/img.height;
-            Ray ray = cam.getRay(u, v);
-            Hit hit;
+    #pragma omp parallel for
+    for (int i = 0; i < 100; i++)
+    {
+        for (int y = 1; y <= img.height; y++)
+        {
+            for (int x = 1; x <= img.width; x++)
+            {
+                float u = (2.0 * (x+rnd()) - img.width) / img.width;
+                float v = (2.0 * (y+rnd()) - img.height) / img.height;
+                Ray ray = cam.getRay(u, v);
+                Hit hit;
+                Vec color;
 
-            if(accel.intersect(ray, hit)) {
-                Ray shadowRay = Ray(hit.hitPos+hit.hitNorm*0.001, lightDir);
-                Hit hit_shadow;
+                if (accel.intersect(ray, hit))
+                {
+                    Ray shadowRay = Ray(hit.hitPos + hit.hitNorm * 0.01, lightDir);
+                    Hit hit_shadow;
 
-                if(!accel.intersect(shadowRay, hit_shadow)) {
-                    float I = std::max(lightDir.dot(hit.hitNorm), (float)0.);
-                    img.setPixel(x, y, Vec(1., 1., 1.)*I);
+                    if (!accel.intersect(shadowRay, hit_shadow))
+                    {
+                        float I = std::max(lightDir.dot(hit.hitNorm), (float)0.);
+                        color = Vec(1., 1., 1.) * I;
 
-                    // img.setPixel(x, y, (hit.hitNorm + Vec(1., 1., 1.))/2.0 );
-                    // std::cout 
-                    //     << ((hit.hitNorm + Vec(1., 1., 1.))/2.0).x 
-                    //     << " " 
-                    //     << ((hit.hitNorm + Vec(1., 1., 1.))/2.0).y 
-                    //     << " " 
-                    //     << ((hit.hitNorm + Vec(1., 1., 1.))/2.0).z 
-                    //     << "\n"; 
-                } else {
-                   img.setPixel(x, y, Vec(0., 0., 0.));
+                        // img.setPixel(x, y, (hit.hitNorm + Vec(1., 1., 1.))/2.0 );
+                        // std::cout
+                        //     << ((hit.hitNorm + Vec(1., 1., 1.))/2.0).x
+                        //     << " "
+                        //     << ((hit.hitNorm + Vec(1., 1., 1.))/2.0).y
+                        //     << " "
+                        //     << ((hit.hitNorm + Vec(1., 1., 1.))/2.0).z
+                        //     << "\n";
+                    }
+                    else
+                    {
+                        color = Vec(0., 0., 0.);
+                    }
                 }
-            } else {
-                img.setPixel(x, y, Vec(0., 0., 0.));
+                else
+                {
+                    color = Vec(0., 0., 0.);
+                }
+
+                img.setPixel(x, y, img.getPixel(x, y)+color/100);
             }
         }
     }
