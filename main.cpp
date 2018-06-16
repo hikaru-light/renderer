81c4dd7729e7fca4f44aea6af1ea672422f6596f
Hikaru Suido <hikaru.suido@gmail.com>
2018-06-16 14:04:07 +0900

    Update

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@
 #include "hit.h"
 #include "accel.h"
 
-// Vec reflect(const Vec& d, const Vec& n) {
-//     return d - n * 2 * d.dot(n);
-// }
+Vec reflect(const Vec& d, const Vec& n) {
+    return d - n * 2 * d.dot(n);
+}
 
 int main() {
     Image img(512, 512);
@@ -20,9 +20,9 @@ int main() {
     Sphere sph(3.0, Vec(0., 0., 3.));
     Accel accel;
     accel.add(std::make_shared<Sphere>(Sphere(1., Vec(0, 0, 0))));
-    accel.add(std::make_shared<Sphere>(Sphere(10000., Vec(0, -10001, 0))));
+    accel.add(std::make_shared<Sphere>(Sphere(1000., Vec(0, -1001, 0))));
 
-    Vec lightDir = Vec(-1, 1, -1).norm();
+    Vec lightDir = Vec(-1, 0.5, -1).norm();
 
     for(int y=1; y<=img.height; y++) {
         for(int x=1; x<=img.width; x++) {
@@ -32,10 +32,24 @@ int main() {
             Hit hit;
 
             if(accel.intersect(ray, hit)) {
-                float I = std::max(lightDir.dot(hit.hitNorm), (float)0.);
-                img.setPixel(x, y, Vec(1., 1., 1.)*I);
-                // img.setPixel(x, y, (hit.hitNorm + Vec(1., 1., 1.))/2.0 );
-                // std::cout << ((hit.hitNorm + Vec(1., 1., 1.))/2.0).x << " " << ((hit.hitNorm + Vec(1., 1., 1.))/2.0).y << " " << ((hit.hitNorm + Vec(1., 1., 1.))/2.0).z << "\n"; 
+                Ray shadowRay = Ray(hit.hitPos+hit.hitNorm*0.001, lightDir);
+                Hit hit_shadow;
+
+                if(!accel.intersect(shadowRay, hit_shadow)) {
+                    float I = std::max(lightDir.dot(hit.hitNorm), (float)0.);
+                    img.setPixel(x, y, Vec(1., 1., 1.)*I);
+
+                    // img.setPixel(x, y, (hit.hitNorm + Vec(1., 1., 1.))/2.0 );
+                    // std::cout 
+                    //     << ((hit.hitNorm + Vec(1., 1., 1.))/2.0).x 
+                    //     << " " 
+                    //     << ((hit.hitNorm + Vec(1., 1., 1.))/2.0).y 
+                    //     << " " 
+                    //     << ((hit.hitNorm + Vec(1., 1., 1.))/2.0).z 
+                    //     << "\n"; 
+                } else {
+                   img.setPixel(x, y, Vec(0., 0., 0.));
+                }
             } else {
                 img.setPixel(x, y, Vec(0., 0., 0.));
             }
